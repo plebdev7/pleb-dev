32e7ea77c7d6bada3d372e25cef29ec6f526b91e
plebdev7@gmail.com <plebdev7@gmail.com>
2023-11-24 23:43:31 +0000

    Add click game UI elements

--- client_code/PageClickGame/TemplateGenerator/__init__.py
+++ client_code/PageClickGame/TemplateGenerator/__init__.py
@@ -1,15 +1,13 @@
-from ._anvil_designer import PageClickGameTemplate  # type: ignore
+from ._anvil_designer import TemplateGeneratorTemplate
+from anvil import *
 
-# from anvil import *
-
-
-class PageClickGame(PageClickGameTemplate):
+class TemplateGenerator(TemplateGeneratorTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
 
-    def button_click_click(self, **event_args):
+    def button_buy_click(self, **event_args):
         """This method is called when the button is clicked"""
         pass
