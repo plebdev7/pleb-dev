b3175ac498ca59c0afdc262cb82cae812c2c5a2e
Pleb <plebdev7@gmail.com>
2023-11-24 03:53:19 +0000

    Nov 23, 2023, 10:53 PM

--- client_code/PageGamesList/__init__.py
+++ client_code/PageGamesList/__init__.py
@@ -1,16 +1,17 @@
-from ._anvil_designer import PageGamesListTemplate
-from anvil import *
+from ._anvil_designer import PageGamesListTemplate  # type: ignore
+from anvil import get_open_form
 
 from ..PageHandler import Page
 
+
 class PageGamesList(PageGamesListTemplate):
-    def __init__(self, **properties):             
+    def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
     def setup(self):
         """Call during form init"""
-        
+
         # Attach pages to individual links
         self.button_click.tag.page = Page.ClickGame
         self.button_potions.tag.page = Page.PotionsGame
@@ -22,7 +23,6 @@ class PageGamesList(PageGamesListTemplate):
     def game_link_click(self, **event_args):
         """Generalized click handler for game links"""
         print(self.button_click.tag)
-        
-        page = event_args['sender'].tag.page
+
+        page = event_args["sender"].tag.page
         get_open_form().open_game(page)
-        
